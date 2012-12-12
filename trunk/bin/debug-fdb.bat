ba04d59807978e44bedfff5f88af3e2fce3d8f54
adenisUA@gmail.com <adenisUA@gmail.com@46262d07-e3dd-4f3b-ae4c-a099a7dd7891>
2012-12-12 12:06:16 +0000

    iOS debugging

---
+++ trunk/bin/debug-fdb.bat
@@ -0,0 +1,4 @@
+set PACKAGER_PATH="C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.6\sdks\4.6.0\bin"
+set PATH=%PATH%;%PACKAGER_PATH%
+
+call fdb -p 7936
--- trunk/bin/iphone.bat
+++ trunk/bin/iphone.bat
@@ -1,4 +1,4 @@
 set PACKAGER_PATH="C:\Program Files (x86)\Adobe\AdobeAIRSDK\bin"
 set PATH=%PATH%;%PACKAGER_PATH%
 
-call adt -package -Xverbose -target ipa-test -provisioning-profile certificate\mobileprovision.mobileprovision -storetype pkcs12 -keystore certificate\p12.p12 -storepass 1234 fc.ipa Application-app.xml Application.swf "Default.png" "icon29.png" "icon57.png" "icon72.png" "icon512.png"
+call adt -package -Xverbose -target ipa-debug -provisioning-profile certificate\mobileprovision.mobileprovision -storetype pkcs12 -keystore certificate\p12.p12 -storepass 1234 fc.ipa app.xml Application.swf "Default.png" "icon29.png" "icon57.png" "icon72.png" "icon512.png"
